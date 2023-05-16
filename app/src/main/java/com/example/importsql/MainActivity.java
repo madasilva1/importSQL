c5c04f93d3725932e3db9907770ffe9eff5d545e
Marcos <madasilva195@gmail.com>
2023-05-16 14:07:05 -0400

    Aplication runing well

--- app/src/main/java/com/example/importsql/MainActivity.java
+++ app/src/main/java/com/example/importsql/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.importsql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText mname;
+    public Button mquery_button;
+    public TextView result_adress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mname = findViewById(R.id.name);
+        mquery_button = findViewById(R.id.query_button);
+        result_adress = findViewById(R.id.resulted);
+
+        mquery_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+                databaseAccess.open();
+                String n= mname.getText().toString();
+                String adress = databaseAccess.getAdress(n);
+                result_adress.setText(adress);
+                databaseAccess.close();
+            }
+        });
     }
 }
